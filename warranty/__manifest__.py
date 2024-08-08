f4abab3bb765626ede0a9c4fca4854f02c17caf2
vava-odoo <vava@odoo.com>
2024-08-08 22:52:38 +0200

    [ADD] warranty: add module to handle warranty
    task-4100027

---
+++ warranty/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Warranty management',
+    'version': '1.0',
+    'category': 'Technical',
+    'description': "",
+    'depends': [
+        'contacts',
+        'project',
+        'sale_subscription',
+        'stock',
+        'web_studio',
+    ],
+    'data': [
+        'data/ir_model.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_ui_view.xml',
+        'data/ir_model_access.xml',
+        'data/res_config_settings.xml',
+    ],
+    'license': 'OPL-1',
+}

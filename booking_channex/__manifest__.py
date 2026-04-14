538eee7647e810f519e959838404a5952cad894a
chga-odoo <chga@odoo.com>
2026-04-14 16:23:23 +0530

    [IMP] *: add channel manager configuration and improve mapping model * = ['booking_engine', 'booking_channex']
    - Add channel manager setting in booking engine
    - Install/uninstall booking_channex module via setting
    - Add API key configuration
    - Improve channex mapping model and views
    - Update module dependencies
    
    Task : 5913892

---
+++ booking_channex/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Booking Channex',
+    'author': 'Odoo S.A.',
+    'category': 'Hidden/Tools',
+    'depends': [
+        'booking_engine',
+    ],
+    'data': [
+        'data/ir_model.xml',
+        'data/ir_model_access.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_actions_server.xml',
+        'data/base_automation.xml',
+        'data/ir_cron.xml',
+        'data/ir_ui_view.xml',
+        'data/ir_ui_menu.xml',
+    ],
+    'demo': [
+        'demo/res_company.xml',
+    ],
+    'license': 'LGPL-3',
+    'cloc_exclude': [
+    ],
+}

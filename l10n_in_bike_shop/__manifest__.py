74e75d8ce605a765b51028d11d5f97c3c267d8f6
dhrs-odoo <dhrs@odoo.com>
2026-07-13 15:00:13 +0530

    test

---
+++ l10n_in_bike_shop/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Indian Localization for Bike Shop",
+    "version": "1.0",
+    "depends": ['l10n_in', 'bike_shop'],
+    "author": "Odoo S.A.",
+    "category": "Localization",
+    "description": """
+        Bridge module to adapt Bike Shop industry module
+        for Indian localization (GST, taxes, accounts).
+    """,
+    "data": [
+        'data/product_template.xml',
+    ],
+    'auto_install': ['l10n_in', 'bike_shop'],
+}
